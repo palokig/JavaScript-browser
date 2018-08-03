67fb243602d858964e7e9c5866e01253a850d06c
pavel <palok@list.ru>
2018-08-03 07:50:28 +0300

    загрузка

--- Lesson4/easter-egg/js/easter.js
+++ Lesson4/easter-egg/js/easter.js
@@ -12,3 +12,47 @@ function showKey() {
 }
 
 document.addEventListener('keydown',showKey);
+
+var secretCode = '';
+var secretCodeCorrect = 'нетология';
+var secretElements = document.getElementsByClassName('secret')[0];
+
+function showSecretCode(){
+
+    let eventCode = event.code;
+    if (event.code === 'KeyY') {
+        eventCode = 'н'
+    }
+    if (event.code === 'KeyT') {
+        eventCode = 'е'
+    }
+    if (event.code === 'KeyN') {
+        eventCode = 'т'
+    }
+    if (event.code === 'KeyJ') {
+        eventCode = 'о'
+    }
+    if (event.code === 'KeyK') {
+        eventCode = 'л'
+    }
+    if (event.code === 'KeyU') {
+        eventCode = 'г'
+    }
+    if (event.code === 'KeyB') {
+        eventCode = 'и'
+    }
+    if (event.code === 'KeyZ') {
+        eventCode = 'я'
+    }
+
+    secretCode = secretCode + eventCode;
+  if (secretCode.length  >= secretCodeCorrect.length ) {
+    if (secretCode === secretCodeCorrect) {
+      secretElements.classList.toggle('visible');
+    }
+    else {
+        SecretCode = '';
+    }
+  }
+}
+document.addEventListener('keydown',showSecretCode);

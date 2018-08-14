540c7eed20d5ea9c01aee7d69b7443ba4a527936
pavel <palok@list.ru>
2018-08-14 21:08:27 +0300

    загрузка

---
+++ lesson8/slider/js/slider.js
@@ -0,0 +1,49 @@
+const sliderNavSelector = document.querySelectorAll('.slider-nav  a');
+
+document.querySelector('.slide').classList.add('slide-current');
+
+function nextActionSlider() {
+    slideCurrentSelector = document.querySelector('.slide-current');
+    if (slideCurrentSelector.nextElementSibling == null){} else {
+    slideCurrentSelector.classList.remove('slide-current');
+    slideCurrentSelector.nextElementSibling.classList.add('slide-current');
+    }
+}
+
+function prevActionSlider() {
+    slideCurrentSelector = document.querySelector('.slide-current');
+    if (slideCurrentSelector.previousElementSibling == null){} else {
+        slideCurrentSelector.classList.remove('slide-current');
+        slideCurrentSelector.previousElementSibling.classList.add('slide-current');
+    }
+}
+
+function firstActionSlider() {
+    slideCurrentSelector = document.querySelector('.slide-current');
+    slideCurrentSelector.classList.remove('slide-current');
+    document.querySelector('.slide').classList.add('slide-current');
+
+}
+
+function lastActionSlider() {
+    slideCurrentSelector = document.querySelector('.slide-current');
+    slideCurrentSelector.classList.remove('slide-current');
+    let slideSelectorAll = document.querySelectorAll('.slide');
+    slideSelectorAll[slideSelectorAll.length-1].classList.add('slide-current');
+}
+
+function slider(container){
+    console.log(container.dataset.action)
+    if (container.dataset.action === 'next') {
+        container.addEventListener('click', nextActionSlider);
+    } else if ( container.dataset.action === 'prev') {
+        container.addEventListener('click', prevActionSlider);
+    }
+    else if ( container.dataset.action === 'first') {
+        container.addEventListener('click', firstActionSlider);
+    }
+    else if ( container.dataset.action === 'last') {
+        container.addEventListener('click', lastActionSlider);
+    }
+}
+Array.from(sliderNavSelector).forEach(item => slider(item));

6a183e38c261aa76e1abc107e349b193254c07d6
pavel <palok@list.ru>
2018-08-15 12:51:59 +0300

    загрузить

--- lesson8/tabs/js/tabs.js
+++ lesson8/tabs/js/tabs.js
@@ -1,18 +1,30 @@
 tabsContentSelectorChildren = document.querySelector('.tabs-content').children;
 var copySample = document.querySelector('.tabs-nav li');
 
-function processingTabs(item) {
+function activateHidden(item) {
     item.classList.add('hidden');
-    console.log(item.dataset.tabTitle);
-    console.log(item.dataset.tabIcon);
+    Array.from(document.querySelectorAll('.tabs-nav li')).forEach(item => {item.classList.remove('ui-tabs-active')});
+}
+
+function processingTabs(item) {
+
     newCopySample = copySample.cloneNode(true);
     newCopySample.children[0].innerText = item.dataset.tabTitle;
-    newCopySample.children[0].classList.add('item.dataset.tabIcon');
+    newCopySample.children[0].classList.add(item.dataset.tabIcon);
+
+    newCopySample.addEventListener('click',()=>{
+        Array.from(tabsContentSelectorChildren).forEach(item => activateHidden(item));
+        item.classList.remove('hidden');
+        Array.from(document.querySelectorAll('.tabs-nav li')).forEach(item => {item.classList.remove('ui-tabs-active')});
+        event.currentTarget.classList.add('ui-tabs-active');
+
+    });
 
     document.querySelector('.tabs-nav').appendChild(newCopySample);
 
 }
 
+Array.from(tabsContentSelectorChildren).forEach(item => activateHidden(item));
 Array.from(tabsContentSelectorChildren).forEach(item => processingTabs(item));
 
 copySample.parentNode.removeChild(copySample);

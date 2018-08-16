c13e8c8cf4251e4b3614412d34a91368a7e18c27
pavel <palok@list.ru>
2018-08-16 12:39:26 +0300

    загрузка

--- lesson9/shop/js/cart.js
+++ lesson9/shop/js/cart.js
@@ -1,1 +1,13 @@
-'use strict';
+'use strict'
+
+
+function clicklist(event) {
+
+    if ( event.target.className == "add-to-cart") {
+        let item = {price: event.target.dataset.price,title: event.target.dataset.title};
+
+        addToCart(item);
+    };
+
+}
+list.addEventListener('click',clicklist);
